79c38b0a6017653fc77a70b9944d0f3e77e0fb60
Jonathan Hager <mail@hager-jonathan.de>
2025-06-09 23:14:50 +0200

    Added debug output when Heap is full

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,8 @@
+#include "Hall/Video.h"
 #include <Hall/Hall.h>
+#include <algorithm>
 #include <fatfs/ff.h>
+#include <memory>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -9,6 +12,35 @@ typedef struct Timo {
   long long2;
   double abcd;
 } Timo;
+
+class Timo2 {
+public:
+  Timo2(int hunger, int energie) : hunger(hunger), energie(energie) {
+    noten.push_back(1.0);
+    noten.push_back(1.7);
+    noten.push_back(2.8);
+    noten.push_back(2.19);
+  }
+
+  ~Timo2() {}
+
+  double besteNote() {
+    return *std::max_element(noten.begin(), noten.end());
+    // double current_max = -1;
+    // for (auto d : noten) {
+    //   if (d < current_max) {
+    //     current_max = d;
+    //   }
+    // }
+
+    // return current_max;
+  }
+
+private:
+  int hunger;
+  int energie;
+  std::vector<double> noten;
+};
 
 int main() {
   // unsigned short data[400*240];
@@ -28,21 +60,26 @@ int main() {
   // UINT br;
   // f_read(&image_file, data, 400*240, &br);
 
-  std::vector<int> hallo{1, 2, 3, 4, 5};
-  hallo.push_back(6);
-  hallo.push_back(7);
-  for (int i : hallo) {
-    printf("%d\n", i);
-  }
+  // Timo *t1 = (Timo *)malloc(sizeof(Timo));
+  // Timo *t2 = (Timo *)malloc(sizeof(Timo));
+  // Timo *t3 = (Timo *)malloc(sizeof(Timo));
+  // Timo *t4 = NULL;
+  // printf("t1: %p, t2: %p, t3: %p, t4: %p\n", t1, t2, t3, t4);
+  // free(t1);
+  // t4 = (Timo *)malloc(sizeof(Timo));
+  // printf("t1: %p, t2: %p, t3: %p, t4: %p\n", t1, t2, t3, t4);
 
-  Timo *t1 = (Timo *)malloc(sizeof(Timo));
-  Timo *t2 = (Timo *)malloc(sizeof(Timo));
-  Timo *t3 = (Timo *)malloc(sizeof(Timo));
-  Timo *t4 = NULL;
-  printf("t1: %p, t2: %p, t3: %p, t4: %p\n", t1, t2, t3, t4);
-  free(t1);
-  t4 = (Timo *)malloc(sizeof(Timo));
-  printf("t1: %p, t2: %p, t3: %p, t4: %p\n", t1, t2, t3, t4);
+  // std::unique_ptr<Timo2> abc = std::make_unique<Timo2>(200, 100);
+  // printf("Die beste Note ist: %f\n", abc->besteNote());
+
+  Hall::Clear(1);
+  while (Hall::GetIsGPUBusy()) {
+  }
+  Hall::SetCommandSwapBuffers();
+  Hall::Clear(1);
+  while (Hall::GetIsGPUBusy()) {
+  }
+  Hall::SetCommandSwapBuffers();
 
   while (true) {
   };
