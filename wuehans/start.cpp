700be338af8afd16c63977256be7d6f8e5a7e70c
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2025-06-27 18:14:22 +0200

    added preinit array initialisation

--- wuehans/start.cpp
+++ wuehans/start.cpp
@@ -4,10 +4,18 @@ typedef void (*constructor_t)(void);
 
 extern constructor_t __init_array_start[];
 extern constructor_t __init_array_end[];
+extern constructor_t __preinit_array_start[];
+extern constructor_t __preinit_array_end[];
 
 void __libc_init_array() 
 {
-    int count = __init_array_end - __init_array_start;
+	int count = __preinit_array_end - __preinit_array_start;
+    for (int i = 0; i < count; i++) 
+	{
+        __preinit_array_start[i]();
+    }
+
+    count = __init_array_end - __init_array_start;
     for (int i = 0; i < count; i++) 
 	{
         __init_array_start[i]();
