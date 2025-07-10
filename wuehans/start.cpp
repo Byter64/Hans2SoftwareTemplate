f92dbcd7a003f3f41f6b78538fe419b86c6cfc3e
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2025-07-10 11:31:17 +0200

    Added bss section set to 0

--- wuehans/start.cpp
+++ wuehans/start.cpp
@@ -2,10 +2,21 @@
 
 typedef void (*constructor_t)(void);
 
+extern unsigned int _sbss;
+extern unsigned int _ebss;
+
 extern constructor_t __init_array_start[];
 extern constructor_t __init_array_end[];
 extern constructor_t __preinit_array_start[];
 extern constructor_t __preinit_array_end[];
+
+static void __init_data()
+{
+    for (unsigned int *data = &_sbss; data < &_ebss; data++) 
+    {
+        *data = 0;
+    }
+}
 
 void __libc_init_array() 
 {
@@ -26,6 +37,7 @@ int main();
 
 void _main()
 {
+    __init_data();
 	__libc_init_array();
 
 	main();
