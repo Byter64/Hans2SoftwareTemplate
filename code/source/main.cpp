9c7d87404bc128fd539332835d1e292a57dda384
Byter <byetr64@gmx.de>
2026-06-13 16:53:34 +0200

    Added: Demo for input and animation Info: Pixel data manipulation demo works now

--- code/source/main.cpp
+++ code/source/main.cpp
@@ -3,10 +3,12 @@
 #include <Hall/Hall.h>
 #include "Image.hpp"
 
-static void ShowCoolTitle();
+static void TitleDemo();
+static void MushroomDemo();
 static void FinishFrame();
 static void Clear(Hall::Color color);
-static void Render(const std::shared_ptr<Image> image, short x, short y);
+static void RenderImage(const std::shared_ptr<Image> image, short x, short y);
+static void RenderSubimage(const std::shared_ptr<Image> image, short x, short y, short offsetX, short offsetY, short width, short height, bool flipX);
 
 static Hall::Color CreateColor(char red, char green, char blue, bool alpha);
 static Hall::Color SetRed(Hall::Color color, char red);
@@ -31,32 +33,21 @@ int main()
 	targetFrameTime = 1 / 30.0f;
 	deltaTime = targetFrameTime;
 
-	ShowCoolTitle();
-	
-	Hall::Color bgColor = CreateColor(4, 1, 4, 1);
-	while (!Hall::ShouldClose())
-	{
-		Clear(bgColor);
-		FinishFrame();
-	}
+	//Show a demo of a static image, of which the image data is manipulated to darken the image
+	TitleDemo();
+
+	//Show a demo of an animated mushroom, that is controllable with WASD
+	MushroomDemo();
 
 	return 0;
 }
 
-void ShowCoolTitle()
+void TitleDemo()
 {
+	std::cout << "TitleDemo started" << std::endl;
+	
 	std::shared_ptr<Image> image = std::make_shared<Image>("assets/byterLogo.bmp");
 
-	std::shared_ptr<Hall::Color[]> data = std::make_shared<Hall::Color[]>(image->width * image->height);
-	for (int i = 0; i < image->width * image->height; i++)
-		data.get()[i] = image->data[i];
-
-	std::shared_ptr<Image> titleScreen = std::make_shared<Image>();
-	titleScreen->width = image->width;
-	titleScreen->height = image->height;
-	titleScreen->data = data;
-
-	std::cout << "CoolTitle started" << std::endl;
 	float timer = 3.0f;
 	float time = 0.7f;
 	int counter = 0;
@@ -68,8 +59,8 @@ void ShowCoolTitle()
 		if (timer <= 0)
 		{
 			// Darken each pixel
-			Hall::Color *color = titleScreen->data.get();
-			for (; color < (titleScreen->data.get() + titleScreen->width * titleScreen->height); color++)
+			Hall::Color *color = image->data.get();
+			for (; color < (image->data.get() + image->width * image->height); color++)
 			{
 				*color = SetRed(*color, GetRed(*color) / 2);
 				*color = SetGreen(*color, GetGreen(*color) / 2);
@@ -82,12 +73,74 @@ void ShowCoolTitle()
 
 		// RENDER CODE
 		Clear(1);
-		Render(titleScreen, 140, 60);
+		RenderImage(image, 140, 60);
 		FinishFrame();
 		// RENDER CODE END
 	}
 
-	std::cout << "CoolTitle finished" << std::endl;
+	std::cout << "TitleDemo ended" << std::endl;
+}
+
+void MushroomDemo()
+{
+	std::cout << "MushroomDemo started" << std::endl;
+
+	std::shared_ptr<Image> image = std::make_shared<Image>("assets/Mushroom-Run.bmp");
+	int x = Hall::SCREEN_WIDTH / 2;
+	int y = Hall::SCREEN_HEIGHT / 2;
+	float speed = 100;
+
+	int animLength = 8; //The animation consists of 8 frames
+	int activeFrame = 0; // Active frame of the animation
+	int frameWidth = image->width / animLength;
+	float animFrameTime = 1.0f / 8.0f; //the inverse of "frames per second" for animation
+	float animTimer = 0;
+
+	Hall::Color bgColor = CreateColor(4, 1, 4, 1);
+	while (!Hall::ShouldClose())
+	{
+		//#############
+		//### Logic ###
+		//#############
+		float speedX = 0;
+		float speedY = 0;
+
+		//Get the input state of the controller with input 0 (== player 1 == left controller)
+		Hall::ControllerState input = Hall::GetController(0);
+		if(Hall::GetLeft(input)) speedX--;
+		if(Hall::GetRight(input)) speedX++;
+		if(Hall::GetUp(input)) speedY--;
+		if(Hall::GetDown(input)) speedY++;
+
+		speedX *= speed;
+		speedY *= speed;
+
+		x += speedX * deltaTime;
+		y += speedY * deltaTime;
+
+		//#################
+		//### Animation ###
+		//#################
+		if(speedX != 0 || speedY != 0)
+		{
+			animTimer += deltaTime;
+			if(animTimer >= animFrameTime)
+			{
+				animTimer -= animFrameTime;
+				activeFrame = (activeFrame + 1) % animLength;
+			}
+		}
+
+		//#################
+		//### Rendering ###
+		//#################
+		Clear(bgColor);
+		RenderSubimage(image, x, y, activeFrame * frameWidth, 0, frameWidth, image->height, speedX > 0);
+
+		FinishFrame();
+	}
+
+	std::cout << "MushroomDemo ended" << std::endl;
 }
 
 void FinishFrame()
@@ -119,8 +172,24 @@ void FinishFrame()
 	currentTime = newTime;
 }
 
+void RenderSubimage(const std::shared_ptr<Image> image, short x, short y, short offsetX, short offsetY, short width, short height, bool flipX)
+{
+	// Wait until GPU is available
+	while (Hall::GetIsGPUBusy())
+		;
 
-void Render(const std::shared_ptr<Image> image, short x, short y)
+	Hall::Color *data = (Hall::Color *)image->data.get();
+	Hall::SetImage(data, image->width, image->height);
+	Hall::SetColorTable(Hall::CTType::NONE);
+	Hall::SetColorSource(Hall::MEMORY);
+	Hall::SetExcerpt(offsetX, offsetY, width, height);
+	Hall::SetScale(1, 1);
+	Hall::SetFlip(flipX, false);
+	Hall::SetScreenPosition(x, y);
+	Hall::Draw();
+}
+
+void RenderImage(const std::shared_ptr<Image> image, short x, short y)
 {
 
 	// Wait until GPU is available
@@ -149,6 +218,7 @@ void Clear(Hall::Color color)
 	Hall::SetColorTable(Hall::NONE);
 	Hall::SetColorSource(Hall::COLOR);
 	Hall::SetShape(Hall::RECTANGLE);
+	Hall::SetExcerpt(0, 0, Hall::SCREEN_WIDTH, Hall::SCREEN_HEIGHT);
 	Hall::SetRectangle(0, 0, Hall::SCREEN_WIDTH, Hall::SCREEN_HEIGHT);
 
 	Hall::Draw();
